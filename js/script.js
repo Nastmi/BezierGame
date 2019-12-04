e8037f4caba7ee0372884b387d5943feb1f9451f
Nastmi <natan.semrl@gmail.com>
2019-12-04 20:17:06 +0100

    Fixed collisions, added proper scaling, end/start point, menu.
    Fixed the collisions this time properly, on resize the stage gets redrawn with proper scaling, added end and star point to stage, reset button. Also a very basic menu.

--- js/script.js
+++ js/script.js
@@ -2,9 +2,13 @@ let mainCanvas;
 let context;
 let arrayOfControlPoints = [];
 let arrayOfLinePoints = [];
-let count=2;
-let animate = true;
 let collisionLines = [];
+let count=2;
+let scaleX = 1;
+let scaleY = 1;
+let startPoint;
+let endPoint;
+let currentStage = 1;
 function drawFromCount(linesToDraw){
     for(let i=0;i<linesToDraw;i++){
         context.beginPath();
@@ -15,8 +19,8 @@ function drawFromCount(linesToDraw){
     }
 }
 function animateDrawCurve(){
-    drawFromCount(15);
-    if(count<arrayOfLinePoints.length && animate){
+    drawFromCount(50);
+    if(count<arrayOfLinePoints.length){
         document.getElementById("drawButton").disabled = true;
         document.getElementById("drawButton").style.opacity = 0.5;
         requestAnimationFrame(animateDrawCurve);
@@ -36,7 +40,7 @@ function createCurveArray(arrControlPoints){
     });
     let chngArray;
     context.strokeStyle = "#1DB954";
-    for(let t=0;t<=1.0;t+=0.001){
+    for(let t=0;t<=1.0;t+=0.0001){
         chngArray = reduceArray(parArray,t);
         arrayOfLinePoints.push(chngArray[0]);
         arrayOfLinePoints.push(chngArray[1]);
@@ -47,50 +51,67 @@ function createCurveArray(arrControlPoints){
     animateDrawCurve(arrayOfLinePoints);
 }
 function drawCurve(arrayToDraw){
+    context.beginPath();
     for(let i=2;i<arrayToDraw.length;i+=2){
-        context.beginPath();
         context.moveTo(arrayToDraw[i-2],arrayToDraw[i-1]);
         context.lineTo(arrayToDraw[i],arrayToDraw[i+1]);
-        context.stroke();
     }
+    context.stroke();
 }
 function checkCollision(x,y){
 	for(let i=0;i<collisionLines.length;i+=4){
-		if(isBetween(Math.trunc(collisionLines[i]),Math.trunc(collisionLines[i+1]),Math.trunc(collisionLines[i+2]),Math.trunc(collisionLines[i+3]),Math.trunc(x),Math.trunc(y)))
+		if(isBetween(collisionLines[i],collisionLines[i+1],collisionLines[i+2],collisionLines[i+3],x,y))
             return true;
     }
     return false;
 }
 function isBetween(x1,y1,x2,y2,x3,y3){
-    /*let crossProduct = (y3-y1)*(x2-x1)-(x3-x1)*(y2-y1);
+    let crossProduct = (y3-y1)*(x2-x1)-(x3-x1)*(y2-y1);
     let dotProduct = (x3-x1)*(x2-x1)+(y3-y1)*(y2-y1);
     let squaredLength = Math.pow((x2-x1),2)+Math.pow(y2-y1,2);
-    if(Math.abs(crossProduct) != 0)
+    //cross product is checked against a tolerance. If working with integers, it would simply need to be != 0, but that is too imprecise.
+    
+    if(Math.abs(crossProduct) > 1000)
         return false;
     if(dotProduct < 0)
         return false
     if(dotProduct > squaredLength)
         return false
-    return true;*/
-    for(let t=0;t<=1.0;t+=0.001){
+    return true;
+
+    /*for(let t=0;t<=1.0;t+=0.0001){
         let chngArray = reduceArray([x1,y1,x2,y2],t);
         if(Math.trunc(x3) == Math.trunc(chngArray[0]) && Math.trunc(y3) == Math.trunc(chngArray[1]))
             return true;
     }
-    return false;
+    return false;*/
 
 }
-function createCollisions(){
-    collisionLines = returnStage(1);
+function createStage(){
+    context.lineWidth = 6;
+    collisionLines = returnStage(currentStage);
 	context.strokeStyle = "#FFFFFF"
     for(let i=0;i<collisionLines.length;i+=4){
         drawCurve([collisionLines[i],collisionLines[i+1],collisionLines[i+2],collisionLines[i+3]]);
     }
+    context.fillStyle = "#1DB954";
+    context.strokeStyle = "#1DB954";
+    context.beginPath();
+    context.arc(startPoint.x, startPoint.y, 10, 0, 2 * Math.PI, false);
+    context.fill();
+    context.stroke();
+    arrayOfControlPoints.push(startPoint.x, startPoint.y);
+    context.beginPath();
+    context.arc(endPoint.x, endPoint.y, 10, 0, 2 * Math.PI, false);
+    context.fill();
+    context.stroke();
+    arrayOfControlPoints.push(endPoint.x, endPoint.y);
+    context.lineWidth = 3;
 }
 function mouseDownFunction(e){
     if(e.button == 0){
-        arrayOfControlPoints.push(e.offsetX);
-        arrayOfControlPoints.push(e.offsetY);
+        arrayOfControlPoints.splice(arrayOfControlPoints.length-2,0,e.offsetX);
+        arrayOfControlPoints.splice(arrayOfControlPoints.length-2,0,e.offsetY);
         context.beginPath();
         context.arc(e.offsetX, e.offsetY, 10, 0, 2 * Math.PI, false);
         context.fillStyle = "#1DB954";
@@ -116,6 +137,8 @@ function initalizeCanvas(){
     mainCanvas = document.getElementById("drawCanvas");
     mainCanvas.width = mainCanvas.clientWidth;
     mainCanvas.height = mainCanvas.clientHeight;
+    scaleX = mainCanvas.clientWidth/1920;
+    scaleY = mainCanvas.clientHeight/799;
     context = mainCanvas.getContext("2d"); 
     mainCanvas.addEventListener("mousedown",function(e){
         mouseDownFunction(e);
@@ -126,15 +149,40 @@ function initalizeCanvas(){
 function onCanvasResize(){
     mainCanvas.width = mainCanvas.clientWidth;
     mainCanvas.height = mainCanvas.clientHeight;
+    scaleX = mainCanvas.clientWidth/1920;
+    scaleY = mainCanvas.clientHeight/799;
+    reset();
 }
 window.onload = function(){
     this.initalizeCanvas();
-    this.createCollisions();
+    this.createStage();
 }
 window.onresize = function(){
     this.onCanvasResize();
 }
 function returnStage(stageNum){
-	if(stageNum == 1)
-		return [200,0,200,400,200,400,900,400,900,400,900,0];
+    console.log(stageNum);
+	if(stageNum == 1){
+        startPoint = {x:250*scaleX,y:200*scaleY};
+        endPoint = {x:1750*scaleX,y:200*scaleY};
+        return [960*scaleX,894*scaleY,960*scaleX,400*scaleY];
+    }
+    else if(stageNum == 2){
+        startPoint = {x:250*scaleX,y:200*scaleY};
+        endPoint = {x:1750*scaleX,y:200*scaleY};
+        return [480*scaleX,0*scaleY,480*scaleX,400*scaleY,960*scaleX,894*scaleY,960*scaleX,400*scaleY,1440*scaleX,0*scaleY,1440*scaleX,400*scaleY];
+    }
+    return null;
+
+}
+function reset(){
+    context.clearRect(0,0,mainCanvas.clientWidth,mainCanvas.clientHeight);
+    arrayOfControlPoints = [];
+    arrayOfLinePoints = [];
+    collisionLines = [];
+    createStage();
+}
+function next(){
+    currentStage+=1;
+    reset();
 }
